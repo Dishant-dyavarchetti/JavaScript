a8dbdc4feab2bb9fecc80e6cdf6b6fa0db488f55
Dishant Dyavarchetti <ddishantmsccs2023@gmail.com>
2024-10-11 00:19:17 +0530

    add more files of js and html

---
+++ array.js
@@ -0,0 +1,38 @@
+// let fname = ["Dishant","Jimit","Vatsal","moxit","Bhavya","Dhruvi"];
+// for(let i=0; i < fname.length ; i++){
+//     console.log(fname[i]);
+// }
+
+// let cities = ["ahmedabad","Surat","Rajkot","Bhavnagar"];
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+// let marks = [85,97,44,37,76,60];
+// let avg = 0
+// for(let i = 0;i< marks.length ; i++){
+//     avg = avg + marks[i];
+// }
+// avg = avg/marks.length
+// console.log(`The average marks of the class is ${avg}`);
+
+// let price = [250,645,300,900,50];
+
+// for(let i = 0; i < price.length; i++){
+//     price[i] = price[i] - (price[i] * 10 / 100);
+// }
+// console.log(`new price ${price}`);
+
+
+//array methods
+// let company = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+// console.log(company.shift())
+// console.log(company.toString());
+
+// company.splice(1,1,"Ola");
+// console.log(company.toString());
+
+
+// company.push("Amazon");
+// console.log(company.toString());
---
+++ first.js
@@ -0,0 +1,15 @@
+// let gamenum = 69
+// do{
+//     num = prompt("Enter the number:");
+//     if(gamenum == num){
+//         console.log("Matched");
+//     }
+//     else{
+//         console.log("not matched");
+//     }
+// }
+// while(gamenum != num);
+
+let str = prompt("Enter the name for username:");
+let username = "@" + str + str.length;
+console.log(username);
