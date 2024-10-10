6e3298286c02dd81affb97000f21b77e0c0f2696
Dishant Dyavarchetti <ddishantmsccs2023@gmail.com>
2024-10-11 02:00:30 +0530

    modified function.js

--- function.js
+++ function.js
@@ -1,8 +1,43 @@
-let a = "Dishant"
-function abcd(a){
-    a = "maa chuda";
-    return a
+// let a = "Dishant"
+// function abcd(a){
+//     a = "maa chuda";
+//     return a
+// }
+
+// b = abcd(a);
+// console.log(b);
+
+
+// //modern js
+// const arrowSum = (a ,b) => {
+//     return a + b;
+// };
+
+// console.log(arrowSum(3,4));
+
+// function isVowel(str) {
+//     let count = 0;
+//     for(let letter of str) {
+//         if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+const isVowel = (str) => {
+    let count = 0;
+    for(let letter of str) {
+        if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+            count++;
+        }
+    }
+    return count;
 }
 
-b = abcd(a);
-console.log(b);
+let str = prompt("Enter the string");
+let vowel = isVowel(str);
+console.log(`the total vowel in the string ${str} is ${vowel}`);
+
+
